7bdfe47c2c61e58fada693126bb7c0a3d2398a0a
Gabriele Giordano <giordano.gabriele@outlook.it>
2023-09-16 15:11:01 +0200

    aggiunte

---
+++ Classification.py
@@ -0,0 +1,150 @@
+import pandas as pd
+import numpy as np
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+import random
+import warnings
+import joblib
+
+warnings.filterwarnings('ignore')
+
+df = pd.read_csv('nilm/nilm/anonimized/25day_dataset.csv', sep=',')
+df_small = df[
+    ['ActivePower', 'ReactivePower', 'Voltage', 'Current', 'harmonic1_Real', 'harmonic1_Imaginary',
+     'harmonic3_Real',
+     'harmonic3_Imaginary', 'harmonic5_Real', 'harmonic5_Imaginary', 'wahing_machine', 'dishwasher', 'oven']]
+
+df_small = df_small.drop(
+  df_small[(df_small['wahing_machine'] == 0) & (df_small['dishwasher'] == 0) & (df_small['oven'] == 0)].sample(
+      frac=0.98).index)
+
+df_small.loc[:, 'wahing_machine'] = df_small['wahing_machine'].apply(lambda x: 1 if x > 0 else 0)
+df_small.loc[:, 'dishwasher'] = df_small['dishwasher'].apply(lambda x: 1 if x > 0 else 0)
+df_small.loc[:, 'oven'] = df_small['oven'].apply(lambda x: 1 if x > 0 else 0)
+
+washing_machine_models = []
+dishwasher_models = []
+oven_models = []
+off_models = []
+scaler = []
+
+for i in range(1, 11):
+    washing_machine_models.append(joblib.load('Models/RF_washing_machine_' + str(i) + '.pkl'))
+    dishwasher_models.append(joblib.load('Models/RF_dishwasher_' + str(i) + '.pkl'))
+    oven_models.append(joblib.load('Models/RF_oven_' + str(i) + '.pkl'))
+    off_models.append(joblib.load('Models/RF_off_' + str(i) + '.pkl'))
+    scaler.append(joblib.load('Models/scaler_' + str(i) + '.pkl'))
+
+y_test = df_small[['wahing_machine', 'dishwasher', 'oven']]
+y_test['off'] = y_test.apply(lambda x: 1 if x['wahing_machine'] == 0 and x['dishwasher'] == 0 and x['oven'] == 0 else 0, axis=1)
+X_test = df_small.drop(['wahing_machine', 'dishwasher', 'oven'], axis=1)
+
+results_ME = pd.DataFrame(columns=['Class', 'Accuracy', 'Precision', 'Recall'])
+
+washing_machine_predictions = pd.DataFrame(np.zeros((len(y_test), 10)))
+dishwasher_predictions = pd.DataFrame(np.zeros((len(y_test), 10)))
+oven_predictions = pd.DataFrame(np.zeros((len(y_test), 10)))
+off_predictions = pd.DataFrame(np.zeros((len(y_test), 10)))
+
+washing_machine_prob = pd.DataFrame(np.zeros((len(y_test), 10)))
+dishwasher_prob = pd.DataFrame(np.zeros((len(y_test), 10)))
+oven_prob = pd.DataFrame(np.zeros((len(y_test), 10)))
+off_prob = pd.DataFrame(np.zeros((len(y_test), 10)))
+
+j = 0
+for i in range(0, 10):
+    X_test_norm = scaler[i].transform(X_test)
+    washing_machine_predictions[j] = washing_machine_models[i].predict(X_test_norm)
+    washing_machine_prob[j] = washing_machine_models[i].predict_proba(X_test_norm)[:, 1]
+    dishwasher_predictions[j] = dishwasher_models[i].predict(X_test_norm)
+    dishwasher_prob[j] = dishwasher_models[i].predict_proba(X_test_norm)[:, 1]
+    oven_predictions[j] = oven_models[i].predict(X_test_norm)
+    oven_prob[j] = oven_models[i].predict_proba(X_test_norm)[:, 1]
+    off_predictions[j] = off_models[i].predict(X_test_norm)
+    off_prob[j] = off_models[i].predict_proba(X_test_norm)[:, 1]
+    j += 1
+
+washing_machine_final_predictions = pd.DataFrame(np.zeros((len(y_test), 1)))
+dishwasher_final_predictions = pd.DataFrame(np.zeros((len(y_test), 1)))
+oven_final_predictions = pd.DataFrame(np.zeros((len(y_test), 1)))
+off_final_predictions = pd.DataFrame(np.zeros((len(y_test), 1)))
+
+washing_machine_final_prob = pd.DataFrame(np.zeros((len(y_test), 1)))
+dishwasher_final_prob = pd.DataFrame(np.zeros((len(y_test), 1)))
+oven_final_prob = pd.DataFrame(np.zeros((len(y_test), 1)))
+off_final_prob = pd.DataFrame(np.zeros((len(y_test), 1)))
+
+for i in range(0, len(X_test)):
+    washing_machine_final_predictions.iloc[i] = washing_machine_predictions.iloc[i].value_counts().index[0]
+    dishwasher_final_predictions.iloc[i] = dishwasher_predictions.iloc[i].value_counts().index[0]
+    oven_final_predictions.iloc[i] = oven_predictions.iloc[i].value_counts().index[0]
+    off_final_predictions.iloc[i] = off_predictions.iloc[i].value_counts().index[0]
+
+    washing_machine_final_prob.iloc[i] = washing_machine_prob.iloc[i].mean()
+    dishwasher_final_prob.iloc[i] = dishwasher_prob.iloc[i].mean()
+    oven_final_prob.iloc[i] = oven_prob.iloc[i].mean()
+    off_final_prob.iloc[i] = off_prob.iloc[i].mean()
+
+y_pred = pd.concat([washing_machine_final_predictions, dishwasher_final_predictions, oven_final_predictions, off_final_predictions], axis=1)
+
+y_pred_final = pd.DataFrame(np.zeros((len(y_pred), 4)), columns=['wahing_machine', 'dishwasher', 'oven', 'Off'])
+
+y_prob = pd.concat([washing_machine_final_prob, dishwasher_final_prob, oven_final_prob, off_final_prob], axis=1)
+
+y_test = y_test.reset_index(drop=True)
+
+for i in range(len(y_pred)):
+    if y_pred.iloc[i].sum() == 1:
+        y_pred_final.iloc[i] = y_pred.iloc[i]
+    else:
+        y_prob_row = y_prob.iloc[i]
+        y_prob_row[y_prob_row == y_prob_row.max()] = 1
+        y_prob_row[y_prob_row != 1] = 0
+        if y_prob_row.sum() > 1:
+            if y_prob_row['Off'] == 1:
+                y_prob_row['Washing Machine'] = 0
+                y_prob_row['Dishwasher'] = 0
+                y_prob_row['Oven'] = 0
+            else:
+                print(i)
+                indici = y_prob_row[y_prob_row == 1].index
+                indice = random.choice(indici)
+                y_prob_row[y_prob_row == 1] = 0
+                y_prob_row[indice] = 1
+        y_pred_final.iloc[i] = y_prob_row
+y_pred_final = y_pred_final.drop('Off', axis=1)
+
+y_test = y_test.drop('off', axis=1)
+accuracy_final = accuracy_score(y_test, y_pred_final)
+precision_final = precision_score(y_test, y_pred_final, average='macro')
+recall_final = recall_score(y_test, y_pred_final, average='macro')
+
+accuracy_wm = accuracy_score(y_test['wahing_machine'], y_pred_final['wahing_machine'])
+precision_wm = precision_score(y_test['wahing_machine'], y_pred_final['wahing_machine'])
+recall_wm = recall_score(y_test['wahing_machine'], y_pred_final['wahing_machine'])
+
+accuracy_dw = accuracy_score(y_test['dishwasher'], y_pred_final['dishwasher'])
+precision_dw = precision_score(y_test['dishwasher'], y_pred_final['dishwasher'])
+recall_dw = recall_score(y_test['dishwasher'], y_pred_final['dishwasher'])
+
+accuracy_ov = accuracy_score(y_test['oven'], y_pred_final['oven'])
+precision_ov = precision_score(y_test['oven'], y_pred_final['oven'])
+recall_ov = recall_score(y_test['oven'], y_pred_final['oven'])
+
+results_ME = results_ME.append({'Class': 'Total', 'Accuracy': accuracy_final,
+                                'Precision': precision_final, 'Recall': recall_final}, ignore_index=True)
+results_ME = results_ME.append({'Class': 'Washing Machine', 'Accuracy': accuracy_wm,
+                                'Precision': precision_wm, 'Recall': recall_wm}, ignore_index=True)
+results_ME = results_ME.append({'Class': 'Dishwasher', 'Accuracy': accuracy_dw,
+                                'Precision': precision_dw, 'Recall': recall_dw}, ignore_index=True)
+results_ME = results_ME.append({'Class': 'Oven', 'Accuracy': accuracy_ov,
+                                'Precision': precision_ov, 'Recall': recall_ov}, ignore_index=True)
+
+results_ME['F1'] = 2 * (results_ME['Precision'] * results_ME['Recall']) / (
+        results_ME['Precision'] + results_ME['Recall'])
+
+results_ME_mean = results_ME.groupby(['Class']).mean()
+
+results_ME.to_csv('Results/Results_ME_final.csv', index_label=results_ME.index.name)
+results_ME_mean.to_csv('Results/Results_ME_mean_final.csv', index_label=results_ME_mean.index.name)
+
+
---
+++ multiexpert.py
@@ -0,0 +1,213 @@
+import time
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import KFold
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, classification_report
+import random
+import warnings
+import joblib
+
+warnings.simplefilter(action='ignore')
+
+df = pd.read_csv('nilm/nilm/anonimized/25day_dataset.csv', sep=',')
+timestamps = df.index.tolist()
+
+df_small = df[
+    ['ActivePower', 'ReactivePower', 'Voltage', 'Current', 'harmonic1_Real', 'harmonic1_Imaginary',
+     'harmonic3_Real',
+     'harmonic3_Imaginary', 'harmonic5_Real', 'harmonic5_Imaginary', 'wahing_machine', 'dishwasher', 'oven']]
+
+df_small.loc[:, 'wahing_machine'] = df_small['wahing_machine'].apply(lambda x: 1 if x > 0 else 0)
+df_small.loc[:, 'dishwasher'] = df_small['dishwasher'].apply(lambda x: 1 if x > 0 else 0)
+df_small.loc[:, 'oven'] = df_small['oven'].apply(lambda x: 1 if x > 0 else 0)
+
+harmonic1_mod = np.sqrt(df['harmonic1_Real'] ** 2 + df['harmonic1_Imaginary'] ** 2)
+harmonic3_mod = np.sqrt(df['harmonic3_Real'] ** 2 + df['harmonic3_Imaginary'] ** 2)
+harmonic5_mod = np.sqrt(df['harmonic5_Real'] ** 2 + df['harmonic5_Imaginary'] ** 2)
+
+df_small['harmonic1_mod'] = harmonic1_mod
+df_small['harmonic3_mod'] = harmonic3_mod
+df_small['harmonic5_mod'] = harmonic5_mod
+
+'''
+df_small.loc[:, 'wahing_machine'] = df_small['wahing_machine'].apply(lambda x: 1 if x > 0 else 0)
+df_small.loc[:, 'dishwasher'] = df_small['dishwasher'].apply(lambda x: 1 if x > 0 else 0)
+df_small.loc[:, 'oven'] = df_small['oven'].apply(lambda x: 1 if x > 0 else 0)
+'''
+
+kf = KFold(n_splits=10, shuffle=True)
+kf.get_n_splits(df_small)
+results_RF = pd.DataFrame(columns=['Class', 'Accuracy', 'Precision', 'Recall'])
+results_ME = pd.DataFrame(columns=['Class', 'Accuracy', 'Precision', 'Recall'])
+k = 1
+init = time.time()
+for train_index, test_index in kf.split(df_small):
+
+    X_train, X_test = df_small.iloc[train_index], df_small.iloc[test_index]
+    '''
+    X_train = X_train.drop(
+        X_train[(X_train['wahing_machine'] == 0) & (X_train['dishwasher'] == 0) & (X_train['oven'] == 0)].sample(
+            frac=0.6).index)
+    '''
+    y_train_washing_machine = X_train['wahing_machine']
+    y_train_dishwasher = X_train['dishwasher']
+    y_train_oven = X_train['oven']
+    # creiamo le label per il classificatore, che sarà 1 se nessuno dei tre è acceso e 0 altrimenti
+    y_train_off = X_train.apply(
+        lambda x: 1 if x['wahing_machine'] == 0 and x['dishwasher'] == 0 and x['oven'] == 0 else 0,
+        axis=1)
+
+    y_test_washing_machine = X_test['wahing_machine']
+    y_test_dishwasher = X_test['dishwasher']
+    y_test_oven = X_test['oven']
+    y_test_off = X_test.apply(
+        lambda x: 1 if x['wahing_machine'] == 0 and x['dishwasher'] == 0 and x['oven'] == 0 else 0,
+        axis=1)
+
+    X_train = X_train.drop(['wahing_machine', 'dishwasher', 'oven'], axis=1)
+    X_test = X_test.drop(['wahing_machine', 'dishwasher', 'oven'], axis=1)
+
+    scaler = MinMaxScaler()
+    scaler.fit(X_train)
+    X_train_norm = scaler.transform(X_train)
+    X_test_norm = scaler.transform(X_test)
+
+    print('Random Forest Washing Machine')
+    rf_washing_machine = RandomForestClassifier(n_estimators=20, random_state=0)
+    rf_washing_machine.fit(X_train_norm, y_train_washing_machine)
+    y_pred_washing_machine = rf_washing_machine.predict(X_test_norm)
+    accuracy_wm_rf = accuracy_score(y_test_washing_machine, y_pred_washing_machine)
+    precision_wm_rf = precision_score(y_test_washing_machine, y_pred_washing_machine)
+    recall_wm_rf = recall_score(y_test_washing_machine, y_pred_washing_machine)
+    washing_machine_prob = rf_washing_machine.predict_proba(X_test_norm)
+
+    print('Random Forest Dishwasher')
+    rf_dishwasher = RandomForestClassifier(n_estimators=20, random_state=0)
+    rf_dishwasher.fit(X_train_norm, y_train_dishwasher)
+    y_pred_dishwasher = rf_dishwasher.predict(X_test_norm)
+    accuracy_dw_rf = accuracy_score(y_test_dishwasher, y_pred_dishwasher)
+    precision_dw_rf = precision_score(y_test_dishwasher, y_pred_dishwasher)
+    recall_dw_rf = recall_score(y_test_dishwasher, y_pred_dishwasher)
+    dishwasher_prob = rf_dishwasher.predict_proba(X_test_norm)
+
+    print('Random Forest Oven')
+    rf_oven = RandomForestClassifier(n_estimators=20, random_state=0)
+    rf_oven.fit(X_train_norm, y_train_oven)
+    y_pred_oven = rf_oven.predict(X_test_norm)
+    accuracy_ov_rf = accuracy_score(y_test_oven, y_pred_oven)
+    precision_ov_rf = precision_score(y_test_oven, y_pred_oven)
+    recall_ov_rf = recall_score(y_test_oven, y_pred_oven)
+    oven_prob = rf_oven.predict_proba(X_test_norm)
+
+    print('Random Forest Off')
+    rf_off = RandomForestClassifier(n_estimators=20, random_state=0)
+    rf_off.fit(X_train_norm, y_train_off)
+    y_pred_off = rf_off.predict(X_test_norm)
+    accuracy_off_rf = accuracy_score(y_test_off, y_pred_off)
+    precision_off_rf = precision_score(y_test_off, y_pred_off)
+    recall_off_rf = recall_score(y_test_off, y_pred_off)
+    off_prob = rf_off.predict_proba(X_test_norm)
+
+    results_RF = results_RF.append({'Class': 'Washing Machine', 'Accuracy': accuracy_wm_rf,
+                                    'Precision': precision_wm_rf, 'Recall': recall_wm_rf}, ignore_index=True)
+    results_RF = results_RF.append({'Class': 'Dishwasher', 'Accuracy': accuracy_dw_rf,
+                                    'Precision': precision_dw_rf, 'Recall': recall_dw_rf}, ignore_index=True)
+    results_RF = results_RF.append({'Class': 'Oven', 'Accuracy': accuracy_ov_rf,
+                                    'Precision': precision_ov_rf, 'Recall': recall_ov_rf}, ignore_index=True)
+    results_RF = results_RF.append({'Class': 'Off', 'Accuracy': accuracy_off_rf,
+                                    'Precision': precision_off_rf, 'Recall': recall_off_rf}, ignore_index=True)
+
+    # Creo un dataframe con le predizioni dei quattro classificatori come colonne
+    y_pred = pd.DataFrame({'Washing Machine': y_pred_washing_machine, 'Dishwasher': y_pred_dishwasher,
+                           'Oven': y_pred_oven, 'Off': y_pred_off})
+
+    y_pred_final = pd.DataFrame(np.zeros((len(y_pred), 4)), columns=['wahing_machine', 'dishwasher', 'oven', 'Off'])
+
+    y_prob = pd.DataFrame({'Washing Machine': washing_machine_prob[:, 1], 'Dishwasher': dishwasher_prob[:, 1],
+                           'Oven': oven_prob[:, 1], 'Off': off_prob[:, 1]})
+
+    y_test = pd.DataFrame({'wahing_machine': y_test_washing_machine, 'dishwasher': y_test_dishwasher,
+                           'oven': y_test_oven})
+    y_test = y_test.reset_index(drop=True)
+
+    for i in range(len(y_pred)):
+        if y_pred.iloc[i].sum() == 1:
+            y_pred_final.iloc[i] = y_pred.iloc[i]
+        else:
+            y_prob_row = y_prob.iloc[i]
+            y_prob_row[y_prob_row == y_prob_row.max()] = 1
+            y_prob_row[y_prob_row != 1] = 0
+            if y_prob_row.sum() > 1:
+                if y_prob_row['Off'] == 1:
+                    y_prob_row['Washing Machine'] = 0
+                    y_prob_row['Dishwasher'] = 0
+                    y_prob_row['Oven'] = 0
+                else:
+                    print(i)
+                    indici = y_prob_row[y_prob_row == 1].index
+                    indice = random.choice(indici)
+                    y_prob_row[y_prob_row == 1] = 0
+                    y_prob_row[indice] = 1
+            y_pred_final.iloc[i] = y_prob_row
+
+    y_pred_final = y_pred_final.drop('Off', axis=1)
+
+    accuracy_final = accuracy_score(y_test, y_pred_final)
+    precision_final = precision_score(y_test, y_pred_final, average='macro')
+    recall_final = recall_score(y_test, y_pred_final, average='macro')
+
+    accuracy_wm = accuracy_score(y_test_washing_machine, y_pred_final['wahing_machine'])
+    precision_wm = precision_score(y_test_washing_machine, y_pred_final['wahing_machine'])
+    recall_wm = recall_score(y_test_washing_machine, y_pred_final['wahing_machine'])
+    accuracy_dw = accuracy_score(y_test_dishwasher, y_pred_final['dishwasher'])
+    precision_dw = precision_score(y_test_dishwasher, y_pred_final['dishwasher'])
+    recall_dw = recall_score(y_test_dishwasher, y_pred_final['dishwasher'])
+    accuracy_ov = accuracy_score(y_test_oven, y_pred_final['oven'])
+    precision_ov = precision_score(y_test_oven, y_pred_final['oven'])
+    recall_ov = recall_score(y_test_oven, y_pred_final['oven'])
+
+    results_ME = results_ME.append({'Class': 'Total', 'Accuracy': accuracy_final,
+                                    'Precision': precision_final, 'Recall': recall_final}, ignore_index=True)
+    results_ME = results_ME.append({'Class': 'Washing Machine', 'Accuracy': accuracy_wm,
+                                    'Precision': precision_wm, 'Recall': recall_wm}, ignore_index=True)
+    results_ME = results_ME.append({'Class': 'Dishwasher', 'Accuracy': accuracy_dw,
+                                    'Precision': precision_dw, 'Recall': recall_dw}, ignore_index=True)
+    results_ME = results_ME.append({'Class': 'Oven', 'Accuracy': accuracy_ov,
+                                    'Precision': precision_ov, 'Recall': recall_ov}, ignore_index=True)
+    joblib.dump(rf_washing_machine, 'Models/RF_washing_machine_' + str(k) + '.pkl')
+    joblib.dump(rf_dishwasher, 'Models/RF_dishwasher_' + str(k) + '.pkl')
+    joblib.dump(rf_oven, 'Models/RF_oven_' + str(k) + '.pkl')
+    joblib.dump(rf_off, 'Models/RF_off_' + str(k) + '.pkl')
+    joblib.dump(scaler, 'Models/Scaler_' + str(k) + '.pkl')
+    k = k + 1
+end = time.time()
+print("Tempo: ", (end - init) / 60, " minuti")
+
+results_RF['F1'] = 2 * (results_RF['Precision'] * results_RF['Recall']) / (
+        results_RF['Precision'] + results_RF['Recall'])
+results_ME['F1'] = 2 * (results_ME['Precision'] * results_ME['Recall']) / (
+        results_ME['Precision'] + results_ME['Recall'])
+
+results_RF_mean = results_RF.groupby(['Class']).mean()
+results_ME_mean = results_ME.groupby(['Class']).mean()
+
+# Voglio calcolare anche le deviazioni standard e metterle accanto alle medie
+results_RF_std = results_RF.groupby(['Class']).std()
+results_ME_std = results_ME.groupby(['Class']).std()
+
+results_RF_mean.insert(1, 'Accuracy std', results_RF_std['Accuracy'])
+results_RF_mean.insert(3, 'Precision std', results_RF_std['Precision'])
+results_RF_mean.insert(5, 'Recall std', results_RF_std['Recall'])
+results_RF_mean.insert(7, 'F1 std', results_RF_std['F1'])
+
+results_ME_mean.insert(1, 'Accuracy std', results_ME_std['Accuracy'])
+results_ME_mean.insert(3, 'Precision std', results_ME_std['Precision'])
+results_ME_mean.insert(5, 'Recall std', results_ME_std['Recall'])
+results_ME_mean.insert(7, 'F1 std', results_ME_std['F1'])
+
+results_RF.to_csv('Results/Results_RF.csv', index_label=results_RF.index.name)
+results_RF_mean.to_csv('Results/Results_RF_mean.csv', index_label=results_RF_mean.index.name)
+results_ME.to_csv('Results/Results_ME.csv', index_label=results_ME.index.name)
+results_ME_mean.to_csv('Results/Results_ME_mean.csv', index_label=results_ME_mean.index.name)
